a8b924912e96fc6f891fb3c1acdc72b4bbcb22e3
TANUJADEV1DADISETTY <147792227+TANUJADEV1DADISETTY@users.noreply.github.com>
2024-07-31 21:51:08 +0530

    import java.util.Scanner; public class adding {     public static void main(String[] args)     {         int x,y,z;         Scanner read=new Scanner(System.in);         x=read.nextInt();         y=read.nextInt();         z=x+y;         System.out.println(z);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class adding
+{
+    public static void main(String[] args)
+    {
+        int x,y,z;
+        Scanner read=new Scanner(System.in);
+        x=read.nextInt();
+        y=read.nextInt();
+        z=x+y;
+        System.out.println(z);
+    }
+}
